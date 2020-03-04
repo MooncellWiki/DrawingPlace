87fa46c4b9e071cc455815393604b9b8a0dd4fbd
StarHeartHunt <starheart233@gmail.com>
2020-03-04 22:00:55 +0800

    modify for mooncell

--- app/src/containers/Header/Header.js
+++ app/src/containers/Header/Header.js
@@ -91,7 +91,7 @@ export default class Header extends Component {
                         // <a className="link-login" onClick={onLogin}>登录</a>
                         // </div>
                     }
-                    <a className="header-icon-github" target="_blank" href="https://github.com/4074/DrawingPlace">
+                    <a className="header-icon-github" target="_blank" href="https://github.com/StarHeartHunt">
                         {this.renderGithubIcon()}
                     </a>
                 </div>
